b92b7d5efb49b024aef52541770e0d1c3245a66b
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-04-14 19:58:40 -0300

    Modify Vite config for GitHub Pages
    Updated Vite configuration for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 /**
- * Configuração mínima do Vite para aplicação React.
+ * Configuração do Vite para deploy no GitHub Pages.
+ *
+ * Se o repositório for:
+ * qr-studio-definitivo-modular
+ *
+ * então o base precisa apontar para:
+ * /qr-studio/
  */
 export default defineConfig({
   plugins: [react()],
+  base: "/qr-studio/",
 });
